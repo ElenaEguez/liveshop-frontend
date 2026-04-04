cbfbc19cc583b2cc316201d831a5b48e387c4af0
Elena <eguezrodas@gmail.com>
2026-04-04 08:46:41 -0400

    fix: eliminar ApiInterceptor que causaba doble prefijo /api/v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { LayoutModule } from './layout/layout.module';
 
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { JwtInterceptor } from './jwt.interceptor';
-import { ApiInterceptor } from './core/interceptors/api.interceptor';
 import { environment } from '../environments/environment';
 
 const socketConfig: SocketIoConfig = { url: environment.apiUrl.replace('/api/v1', ''), options: {} };
@@ -28,7 +27,6 @@ const socketConfig: SocketIoConfig = { url: environment.apiUrl.replace('/api/v1'
     SocketIoModule.forRoot(socketConfig)
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
